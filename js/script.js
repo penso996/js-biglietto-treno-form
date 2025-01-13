330edc31043b6413a2bf55f1a889af8928b383da
penso996 <penso.andrea@outlook.it>
2025-01-13 16:53:43 +0100

    working on js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("calculate-button").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const userName = document.getElementById("user-name").value.trim();
+    const kilometers = document.getElementById("user-kilometers").value;
+    const age = document.getElementById("user-age").value;
+
+})
