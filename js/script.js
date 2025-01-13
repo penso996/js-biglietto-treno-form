2913ad3e90df662c55d8a9b8fc9533ea4e2d8cac
penso996 <penso.andrea@outlook.it>
2025-01-13 16:57:17 +0100

    working on js

--- js/script.js
+++ js/script.js
@@ -5,4 +5,15 @@ document.getElementById("calculate-button").addEventListener("click", function (
     const kilometers = document.getElementById("user-kilometers").value;
     const age = document.getElementById("user-age").value;
 
-})
+    const = pricePerKm = 0.21;
+    let = ticketPrice = kilometers * pricePerKm;
+
+    if (age < 18) {
+        ticketPrice = ticketPrice * 0.8;
+    } else if (age >= 65) {
+        ticketPrice = ticketPrice * 0.6;
+    }
+
+    ticketPrice = ticketPrice.toFixed(2);
+
+});
