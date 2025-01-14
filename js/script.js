947027e6eb30b3f4325ae187fdc573d187bd1bce
penso996 <penso.andrea@outlook.it>
2025-01-14 09:57:40 +0100

    added discount label in html and js

--- js/script.js
+++ js/script.js
@@ -9,19 +9,23 @@ document.getElementById("calculate-button").addEventListener("click", function (
     //CALCOLO DEL PREZZO
     const pricePerKm = 0.21;
     let ticketPrice = kilometers * pricePerKm;
+    let discount = "Prezzo pieno";
 
     if (age < 18) {
         ticketPrice = ticketPrice * 0.8;
+        discount = "20%";
     } else if (age >= 65) {
         ticketPrice = ticketPrice * 0.6;
+        discount = "40%";
     }
 
     ticketPrice = ticketPrice.toFixed(2);
 
     //OUTPUT
     document.getElementById("user-name-output").textContent = userName;
-    document.getElementById("user-kilometers-output").textContent = kilometers + " km";
     document.getElementById("user-age-output").textContent = age + " anni";
+    document.getElementById("user-discount-output").textContent = discount;
+    document.getElementById("user-kilometers-output").textContent = kilometers + " km";
     document.getElementById("user-price-output").textContent = "€ " + ticketPrice;
 
 });
